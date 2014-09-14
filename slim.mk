ffceff86b20870d3fd6f70403fa3c1eb7cefef63
olivieer <yves.olli@gmail.com>
2014-09-14 15:08:31 -0400

    yuga: Update build fingerprint (10.5.1.A.0.283)

--- slim.mk
+++ slim.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.4.2/10.5.A.0.230/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="yuga-userdebug 4.4.2 10.5.A.0.230 WP5_rg test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.4.4 10.5.1.A.0.283 5P53rQ release-keys"
 
 PRODUCT_NAME := slim_yuga
 PRODUCT_DEVICE := yuga
